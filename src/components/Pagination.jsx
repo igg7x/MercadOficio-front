3440536bba81d6ebd545e65b52ae72f1aae11409
Ignacio Gonzalez <ignagonzalez2003@gmail.com>
2024-08-09 01:09:34 -0300

    fix: fixing the pagination component addig props and logic

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,73 +1,33 @@
-import { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "../assets/icons/Icons";
 
-const Pagination = () => {
-  const [pages, setPages] = useState(["1", "2", "3", , "...", "8", "9", "10"]);
-  const [currentPage, setCurrentPage] = useState("1");
-
+const Pagination = ({
+  prevPage,
+  nextPage,
+  page,
+  isPreviousData,
+  isDataLast,
+}) => {
   return (
-    <div className="max-w-screen-xl  flex justify-center items-end  mb-4 mx-auto mt-12 px-4 text-gray-600 md:px-8">
-      <div
-        className="hidden items-center justify-between sm:flex"
-        aria-label="Pagination">
-        <a className="hover:text-indigo-600 flex items-center gap-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-5 h-5">
-            <path
-              fillRule="evenodd"
-              d="M18 10a.75.75 0 01-.75.75H4.66l2.1 1.95a.75.75 0 11-1.02 1.1l-3.5-3.25a.75.75 0 010-1.1l3.5-3.25a.75.75 0 111.02 1.1l-2.1 1.95h12.59A.75.75 0 0118 10z"
-              clipRule="evenodd"
-            />
-          </svg>
-          Anterior
-        </a>
-        <ul className="flex items-center gap-1">
-          {pages.map((item, idx) => (
-            <li key={item} className="text-sm">
-              {item == "..." ? (
-                <div>{item}</div>
-              ) : (
-                <a
-                  aria-current={currentPage == item ? "page" : false}
-                  className={`px-3 py-2 rounded-lg duration-150 hover:text-indigo-600 hover:bg-indigo-50 ${
-                    currentPage == item
-                      ? "bg-indigo-50 text-indigo-600 font-medium"
-                      : ""
-                  }`}>
-                  {item}
-                </a>
-              )}
-            </li>
-          ))}
-        </ul>
-        <a className="hover:text-indigo-600 flex items-center gap-x-2">
-          Siguiente
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-5 h-5">
-            <path
-              fillRule="evenodd"
-              d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </a>
-      </div>
-      <div className="flex items-center justify-between text-sm text-gray-600 font-medium sm:hidden">
-        <a className="px-4 py-2 border rounded-lg duration-150 hover:bg-gray-50">
-          Anterior
-        </a>
-        <div className="font-medium">
-          Page {currentPage} of {pages.length}
-        </div>
-        <a className="px-4 py-2 border rounded-lg duration-150 hover:bg-gray-50">
-          Siguiente
-        </a>
-      </div>
+    <div className="flex gap-2 p-4 items-center">
+      <button
+        className={`bg-gray-200  ${
+          page === 0 ? "text-gray-400" : ""
+        } flex gap-1 items-center justify-start  p-1 rounded-md`}
+        onClick={prevPage}
+        disabled={page === 0}>
+        <ChevronLeftIcon />
+        Anterior
+      </button>{" "}
+      <div> Pagina: {page + 1}</div>
+      <button
+        className={`bg-gray-200 flex ${
+          isPreviousData || isDataLast ? "text-gray-400" : ""
+        } gap-1 items-center justify-start p-1 rounded-md`}
+        onClick={nextPage}
+        disabled={isPreviousData || isDataLast}>
+        Siguiente
+        <ChevronRightIcon />
+      </button>
     </div>
   );
 };
