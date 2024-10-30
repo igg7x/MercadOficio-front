681b875d8878a3a3c28b03598ded501e98162aef
Ignacio Gonzalez <ignagonzalez2003@gmail.com>
2024-10-30 16:33:19 -0300

    feat: create a generic modal for the entire application

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex h-[400px] w-full items-center justify-center">
-      <div className="flex items-center space-x-4">
-        <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-transparent dark:border-gray-600" />
-        <p className="text-gray-500 dark:text-gray-400">Cargando...</p>
+      <div className="flex items-center gap-1 justify-center">
+        <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-gray-600"></div>
+        <div>
+          <span className="text-gray-400">Cargando...</span>
+        </div>
       </div>
     </div>
   );
